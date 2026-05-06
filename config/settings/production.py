2e6a84571d5ab1ef616d4bf5ea33b66330c39bf2
Nahin <secretadmire226@gmail.com>
2026-05-06 13:25:04 +0600

    Fix Redis in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,6 +7,19 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+# Use dummy cache instead of Redis in production
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
+
+# Disable throttling cache dependency
+REST_FRAMEWORK = {
+    **REST_FRAMEWORK,
+    "DEFAULT_THROTTLE_CLASSES": [],
+}
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,

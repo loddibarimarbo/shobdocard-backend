71830515532d7c1005c3d267920c12c5357d115c
Nahin <secretadmire226@gmail.com>
2026-05-06 12:07:04 +0600

    Fix SECRET_KEY default for build

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / ".env")
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="temporary-build-key-replace-in-production")
 DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
